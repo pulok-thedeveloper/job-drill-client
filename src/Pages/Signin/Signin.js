142d1febdffa43a86a513ba67a9d8536658f3d76
pulok-thedeveloper <mdpulokhasan@gmail.com>
2023-02-18 18:00:02 +0600

    Sign in page added

--- src/Pages/Signin/Signin.js
+++ src/Pages/Signin/Signin.js
@@ -1,11 +1,80 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthProvider";
 
 const Signin = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    login(email, password)
+      .then((result) => {
+        console.log(result.user);
+        form.reset();
+        navigate(from, { replace: true });
+      })
+      .catch((err) => console.error(err));
+  };
+
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content grid justify-items-center">
+        <form
+          onSubmit={handleLogin}
+          className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 pt-10"
+        >
+          <h1 className="text-3xl font-bold text-center">Login</h1>
+          <div className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                name="email"
+                type="text"
+                placeholder="email"
+                required
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                name="password"
+                type="password"
+                placeholder="password"
+                required
+                className="input input-bordered"
+              />
+              <label className="label">
+                <a href="/" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="mt-5">
+              <p>
+                New User?{" "}
+                <Link className="font-bold" to="/signup">
+                  Create an account
+                </Link>
+              </p>
+            </div>
+            <input type="submit" value="Login" className="btn mt-5" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Signin;
